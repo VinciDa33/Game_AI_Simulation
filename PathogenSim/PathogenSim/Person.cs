25d18d35a313aa923dc1ab809b3a64dc543c7a7a
VinciDa <marcusbthomsen@gmail.com>
2025-11-05 09:45:55 +0100

    Initial project setup

---
+++ PathogenSim/PathogenSim/Person.cs
@@ -0,0 +1,12 @@
+namespace PathogenSim;
+
+public class Person
+{
+    private bool _isInfected;
+
+
+    public bool IsInfected()
+    {
+        return _isInfected;
+    }
+}
---
+++ PathogenSim/PathogenSim/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace PathogenSim;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+}
